87332cf09b7db464a2d8973a565521126c71bbdc
Jozef Méry <jozefdmery@gmail.com>
2020-03-02 22:55:56 +0100

    changed file name, removed language data from state

--- client/src/components/state/languageSlice.js
+++ client/src/components/state/languageSlice.js
@@ -1,5 +1,5 @@
 /**
- * File: langSlice.js
+ * File: languageSlice.js
  * 
  * Author: Jozef Méry <xmeryj00@stud.fit.vutbr.cz>
  * Date: 28.1.2020
@@ -11,18 +11,15 @@
 // import redux toolkit
 import { createSlice } from "@reduxjs/toolkit";
 
-// import languages
-import languages from "../../lang/all";
-
 export default createSlice({
 
-    name: "lang",
+    name: "language",
     initialState: null, // inherit initial state from store
 
     reducers: {
 
         // safe to mutate state thanks to redux toolkit
 
-        "setLanguage": (state, action) => { state.data = languages[action.payload]; }
+        "setLanguage": (_, action) => action.payload
     }
 });
