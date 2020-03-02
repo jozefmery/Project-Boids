a9869e920bd5c290fe4770623f1d1bbe37438b11
Jozef Méry <jozefdmery@gmail.com>
2020-03-02 23:02:55 +0100

    fixed imports, code cleanup

--- client/src/components/ui/Controls.jsx
+++ client/src/components/ui/Controls.jsx
@@ -11,43 +11,47 @@
 // import dependencies
 import React, { Component } from "react";
 import { Capitalize, DispatchToProps } from "../../utils";
+import classNames from "classnames";
 
 // import react-redux
 import { connect } from "react-redux";
 
 // import redux slices
-import langSlice from "../state/langSlice";
+import languageSlice from "../state/languageSlice";
 import simSlice from "../state/simSlice";
 
 // import custom components
 import Button from "./Button";
 
+// import language data
+import LanguageData from "../../lang/data";
+
 class Controls extends Component {
 
     /// Properties
 
-    static stateToProps = state => ({
-
-        lang: state.lang.data,
-        sim: state.sim
-    });
+    static stateToProps = ({ language, sim, theme }) => ({ language, sim, theme });
    
     /// Methods
-
-    toggleSimState = () => this.props.setSimPlayState(!this.props.sim.state.play);
     
-    render() { 
+    render() {
 
-        let text = Capitalize((this.props.sim.state.play) ? this.props.lang["play"] : this.props.lang["pause"]);
+        // --- shorthands 
+        const simState = this.props.sim.running;
+        const languageData = LanguageData[this.props.language];
+        const theme = this.props.theme;
+        // --- shorthands
+
+        let text = Capitalize((simState) ? languageData["play"] : languageData["pause"]);
 
         return (
-            <div id="bottom-controls">
-                    <Button content={() => text} id="" className="button" 
+            <div id="bottom-controls" className={classNames(theme, "ui-panel")}>
+                    <Button content={() => text} id="" classNames={theme} 
                             tooltip={text}
-                            onClick={this.toggleSimState}
+                            onClick={() => this.props.toggleSimRunning()}
                     />
             </div>);
     }
 }
  
-export default connect(Controls.stateToProps, DispatchToProps([simSlice, langSlice]))(Controls);
+export default connect(Controls.stateToProps, DispatchToProps([simSlice, languageSlice]))(Controls);
