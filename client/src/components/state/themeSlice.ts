2457ed7b9fa1d806cfceb57acf48581e32dc3d47
Jozef Méry <jozefdmery@gmail.com>
2020-03-23 17:40:16 +0100

    removed redundant parameter from action

--- client/src/components/state/themeSlice.ts
+++ client/src/components/state/themeSlice.ts
@@ -30,6 +30,6 @@ export default createSlice({
         // safe to mutate state thanks to redux toolkit
 
         setTheme: (_, { payload: theme }) => theme, 
-        toggleTheme: (state, _) => themeInverter[state],
+        toggleTheme: (state) => themeInverter[state],
     }
 });
