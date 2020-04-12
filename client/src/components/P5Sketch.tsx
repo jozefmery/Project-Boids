bdb372ca96b45d845f4740d5317901049aeaa2d2
Jozef Méry <jozefdmery@gmail.com>
2020-04-12 22:03:23 +0200

    converted to hook API, code cleanup

--- client/src/components/P5Sketch.tsx
+++ client/src/components/P5Sketch.tsx
@@ -9,130 +9,88 @@
  */
 
 // import dependencies
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import P5 from "p5";
-import classNames from "classnames";
-
-interface EmptyCallback {
-
-    (): void;
-}
-
-
+import classNamesFunc from "classnames";
 
 type P5SketchProps = {
 
-    classNames?: Parameters<typeof classNames>[0];
-    id?: string;
+    classNames?: Parameters<typeof classNamesFunc>[0];
     // p5 setup
-    preload?: (p5: P5) => void;
-    setup: (p5: P5, parent: HTMLDivElement) => void;
+    preload?: ((p5: P5) => any) | void;
+    setup: (p5: P5, parent: HTMLDivElement) => any;
     // p5 events
-    draw?: EmptyCallback;
-    windowResized?: EmptyCallback;
-    // react events
-    onWheel?: React.EventHandler<React.WheelEvent>;
-    onClick?: React.EventHandler<React.MouseEvent>;
-    onDoubleClick?: React.EventHandler<React.MouseEvent>;
-    onMouseDown?: React.EventHandler<React.MouseEvent>;
-    onMouseUp?: React.EventHandler<React.MouseEvent>;
-    onMouseMove?: React.EventHandler<React.MouseEvent>;
-    onMouseEnter?: React.EventHandler<React.MouseEvent>;
-    onMouseLeave?: React.EventHandler<React.MouseEvent>;
-    onKeyDown?: React.EventHandler<React.KeyboardEvent>;
-    onKeyUp?: React.EventHandler<React.KeyboardEvent>;
-    onTouchStart?: React.EventHandler<React.TouchEvent>;
-    onTouchMove?: React.EventHandler<React.TouchEvent>;
-    onTouchEnd?: React.EventHandler<React.TouchEvent>;
-};
+    draw?: (() => any) | void;
+    windowResized?: (() => any) | void;
 
-export default class P5Sketch extends Component<P5SketchProps> {
+} & Omit<React.ComponentProps<"div">, "className" | "ref">;
 
-    /// Public static members
-
-    public static readonly defaultProps = {
-
-        classNames: "asd",
-        id: "p5-sketch"
-    };
-
-    /// Private members
-
-    private canvasParent: React.RefObject<HTMLDivElement>;
-    private p5: P5 = null as any; // initialized after mounting
-
-    /// Constructor function
-
-    public constructor(props: P5SketchProps) {
-
-        super(props);
-
-        this.canvasParent = React.createRef();
-    }
-
-    /// Private methods
-
-    private instantiateP5() {
-
-        this.p5 = new P5((p5: P5) => {
-
-            if(this.props.preload) {
-
-                // cast to original type
-                // check is provided above
-                p5.preload = () => (this.props.preload as (p5: P5) => void)(p5);
-            }
-            
-            // cast canvasParent to div because it can't be undefined at this point
-            p5.setup = () => this.props.setup(p5, this.canvasParent.current as HTMLDivElement);
-        });
-    }
-
-    private setP5events() {
-
-        // cast to empty callback even if undefined
-        // to enable removing callbacks
-        this.p5.draw = this.props.draw as EmptyCallback;
-        this.p5.windowResized = this.props.windowResized as EmptyCallback;
-    }
-
-    /// Public methods
-
-    public componentDidMount() {
-
-        this.instantiateP5();
-        this.setP5events();
-    }
-    
-    public componentDidUpdate() {
+type P5Events = { draw: P5SketchProps["draw"], windowResized: P5SketchProps["windowResized"] };
         
-        this.setP5events();
-    }
+function useP5(preload: P5SketchProps["preload"], events: P5Events) {
 
-    public componentWillUnmount() {
+    const instance = useRef<P5 | null>(null);
 
-        this.p5.remove();
-    }
+    useEffect(() => {
 
-    public render() {
+        instance.current = new P5((p5: P5) => {
 
-        const { 
-            // extract redundant props
-            preload,
-            setup,
-            windowResized,
-            draw,
-            // extract class list to be called with classNames
-            classNames: classList,
-            // extract rest of event handlers
-            ...props 
-        } = this.props;
+            if(preload) {
+    
+                p5.preload = () => preload(p5);
+            }
+        });
 
-        return <div ref={this.canvasParent} 
-                    className={classNames(classList)}
-                    tabIndex={0} // enable keyboard events
-                    // pass event handlers as is
-                    { ...props }
-                    />;
-    }
-};
+        return () => {
+
+            if(instance.current) {
+
+                instance.current.remove();
+            }
+        }
+
+    }, []);
+
+    useEffect(() => {
+
+        if(instance.current) {
+
+            instance.current.draw = events.draw as () => undefined;
+            instance.current.windowResized = events.windowResized as () => undefined;
+        }
+
+    }, [events.draw, events.windowResized]);
+
+    return instance;
+}
+
+export default function P5Sketch({ classNames = "", ...props }: P5SketchProps) {
+
+    const parent = useRef(null);
+
+    const p5 = useP5(props.preload, { draw: props.draw, windowResized: props.windowResized });
+
+    useEffect(() => {
+
+        if(p5.current !== null && parent.current !== null) {
+
+            props.setup(p5.current, parent.current as any);
+        }
+
+    }, []);
+
+    const { 
+        // extract props which are used elsewhere
+        preload,
+        setup,
+        draw,
+        windowResized,
+        // extract rest of props 
+        ...rest 
+    } = props;
+
+    return <div ref={parent} className={classNamesFunc(classNames)}
+                // pass rest of props as is
+                { ...rest }
+                />;
+}
