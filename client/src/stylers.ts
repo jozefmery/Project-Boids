7a34f5f1bb7f37278e772c242077fa5dc79240e4
Jozef Méry <jozefdmery@gmail.com>
2020-04-12 22:07:37 +0200

    created style helper class, created global styles, added override from MUI Theme to support ColorTheme

--- client/src/stylers.ts
+++ client/src/stylers.ts
@@ -10,13 +10,18 @@
 
 // import dependecies
 import P5 from "p5";
-import { Theme as MUITheme } from "@material-ui/core/styles";
+
+// import utils
+import lodash from "lodash";
+
+// import MUI stylers
+import { CSSProperties as MUICSSProperties } from "@material-ui/styles/withStyles";
 
 /**
  * 
- * Enum representing the possible global application themes, with values representing assigned class names.
+ * Enum representing the possible global application themes.
  */
-export enum Theme {
+export enum ColorTheme {
 
     LIGHT = "light",
     DARK = "dark"
@@ -24,19 +29,32 @@ export enum Theme {
 
 /**
  * 
+ * Array of Theme enum values.
+ */
+export const themeList = Object.values(ColorTheme);
+
+/**
+ * 
  * Extended Material UI theme with a custom variable
  * representing the global theme.
  */
-export interface ThemeData extends MUITheme {
+declare module '@material-ui/core/styles/createMuiTheme' {
+    interface Theme {
 
-    theme: Theme;
-};
+        theme: ColorTheme;
+    }
+    // allow configuration using `createMuiTheme`
+    interface ThemeOptions {
+        
+        theme?: ColorTheme;
+    }
+}
 
 /**
  * 
  * List of properties every styler requires.
  */
-type stylerList =   "background"    |
+type SimStylerList =   "background"    |
                     "area"          |
                     "grid"          |
                     "gridHighlight" |
@@ -46,27 +64,27 @@ type stylerList =   "background"    |
  * 
  * Plain object type which contains every styler function from styler list.
  */
-type Styler = {
+type SimStyler = {
 
-    [styler in stylerList]: (p5: P5) => void;
+    [styler in SimStylerList]: (p5: P5) => void;
 }
 
 /**
  * 
  * Plain object type which contains a Styler object for every theme.
  */
-type IStylers = {
+type SimStylers = {
 
-    [theme in Theme]: Styler;
+    [theme in ColorTheme]: SimStyler;
 }
 
 /**
  * 
  * Styler implementations for every theme.
  */
-const stylers: IStylers = {
+export const simStylers: SimStylers = {
 
-    [Theme.DARK]: {
+    [ColorTheme.DARK]: {
 
         background: (p5) => p5.background(70),
         
@@ -87,12 +105,12 @@ const stylers: IStylers = {
         boundingBox: (p5) => {
 
             p5.noFill();
-            p5.stroke(59, 85, 128);
-            p5.strokeWeight(3);
+            p5.stroke(140);
+            p5.strokeWeight(2);
         }
     },
 
-    [Theme.LIGHT]: {
+    [ColorTheme.LIGHT]: {
 
         background: (p5) => p5.background(200),
 
@@ -119,4 +137,245 @@ const stylers: IStylers = {
     },
 };
 
-export default stylers;
+/**
+ * 
+ * Defines CSS in JS object shape.
+ * Re-export from MUI library.
+ */
+export type CSSProperties = MUICSSProperties;
+
+type ThemedCSSProperties = {
+
+    [theme in ColorTheme]?: CSSProperties;
+};
+
+type NoUndefinedField<T> = { [P in keyof T]-?: NonNullable<T[P]> };
+
+type GlobalStyle = "UIPanel" 
+                    | "ColorTransition" 
+                    | "ControlButton" 
+                    | "VerticalFlexBox" 
+                    | "TopBarButton"
+                    | "SimpleTooltip";
+
+export class Style {
+
+    /// Protected static members
+
+    protected static globalThemedStyles: { [component in GlobalStyle]: Style } = {
+
+        VerticalFlexBox: Style.create({
+
+            display: "flex",
+            flexFlow: "row nowrap",
+            justifyContent: "space-evenly"
+        }),
+
+        ColorTransition: Style.create({
+
+            transition: "background-color .3s linear, color .3s linear, border-color .3s linear",
+        }),
+        
+        UIPanel: Style.create({
+            
+            borderWidth: "1px",
+        }, 
+        {
+    
+            [ColorTheme.DARK]: {
+                
+                
+                borderColor: "#801313",
+
+                boxShadow: "0px 0px 10px rgba(255,255,255,0.1)",
+                backgroundColor: "#292929"
+            },
+        
+            [ColorTheme.LIGHT]: {
+                
+                borderColor: "#801313",
+
+                boxShadow: "0px 0px 10px rgba(255,255,255,0.1)",
+                backgroundColor: "#292929"
+            }
+        }),
+
+        TopBarButton: Style.create({
+
+            minWidth: "0px", // override default
+            padding: "10px",
+            borderRadius: "50%",
+
+            "& svg": {
+
+                fontSize: "30px"
+            }
+        }, 
+        {
+    
+            [ColorTheme.DARK]: {
+                
+                color: "#cecece",
+
+                "&:hover": {
+    
+                    backgroundColor: "#1d1d1d"
+                }
+            },
+        
+            [ColorTheme.LIGHT]: {
+                
+                color: "#cecece",
+
+                "&:hover": {
+    
+                    backgroundColor: "#1d1d1d"
+                }
+            }
+        }),
+
+        ControlButton: Style.create({
+
+            // spacing
+            padding: "5px",
+    
+            // style
+            borderRadius: "3px",
+            
+            // icon style
+            "& svg": {
+    
+                fontSize: "45px"
+            }
+        },
+        {
+    
+            [ColorTheme.DARK]: {
+    
+                color: "#cecece",
+                border: "1px #a8a8a8 solid",
+    
+                "&:hover": {
+    
+                    backgroundColor: "#1d1d1d"
+                }
+            },
+
+            [ColorTheme.LIGHT]: {
+    
+                color: "#cecece",
+                border: "1px #a8a8a8 solid",
+    
+                "&:hover": {
+    
+                    backgroundColor: "#1d1d1d"
+                }
+            }
+        }),
+
+        "SimpleTooltip": Style.create({
+
+            padding: "5px 10px",
+            fontSize: "15px",
+            boxShadow: "0px 0px 5px black"
+        },
+        {
+            [ColorTheme.DARK]: {
+
+                color: "black",
+                backgroundColor: "white"
+
+            },
+            [ColorTheme.LIGHT]: {
+
+                color: "white",
+                backgroundColor: "black"
+            },
+        })
+    };
+
+    /// Protected members
+
+    // members are initialized in ctor using methods 
+    protected shared: CSSProperties = {};
+    protected themed: NoUndefinedField<ThemedCSSProperties> = {
+
+        [ColorTheme.LIGHT]: {},
+        [ColorTheme.DARK]: {}
+    };
+
+    protected globalStyles: Array<GlobalStyle> = [];
+
+    /// Constructor function
+
+    protected constructor(sharedProperties:     Readonly<CSSProperties>, 
+                            themedProperties:   Readonly<ThemedCSSProperties>, 
+                            globalStyles:       Readonly<Array<GlobalStyle>> | GlobalStyle) {
+
+        this.setSharedCSS(sharedProperties);
+        this.setThemedCSS(themedProperties);
+        this.setGlobalStyles(globalStyles)
+    }
+
+    /// Public static methods
+
+    public static create(sharedProperties?: CSSProperties, 
+                            themedProperties?: Readonly<ThemedCSSProperties>, 
+                            globalStyles: Readonly<Array<GlobalStyle>> | GlobalStyle = []): Style {
+
+        return new Style(sharedProperties ? sharedProperties : {}, 
+                        themedProperties ? themedProperties : {},
+                        globalStyles);
+    }
+
+    /// Public methods
+
+    public setSharedCSS(sharedProperties: Readonly<CSSProperties>): void {
+
+        this.shared = lodash.cloneDeep(sharedProperties);
+    }
+    
+    public setThemedCSS(themedProperties: Readonly<ThemedCSSProperties>): void {
+
+        this.themed = lodash.merge({
+
+            [ColorTheme.LIGHT]: {},
+            [ColorTheme.DARK]: {}
+
+        }, themedProperties);
+    }
+
+    public setGlobalStyles(globalStyles: Readonly<Array<GlobalStyle>> | GlobalStyle): void {
+
+        if(typeof globalStyles === "string") {
+
+            this.globalStyles = [ globalStyles ];
+
+        } else {
+
+            this.globalStyles = [ ...globalStyles ];
+        }
+    }
+
+    public clearGlobalStyles(): void {
+
+        this.globalStyles = [];
+    }
+
+    public clearSharedCSS(): void {
+
+        this.setSharedCSS({});
+    }
+
+    public clearThemedCSS(): void {
+
+        this.setSharedCSS({});
+    }
+
+    public compose(theme: ColorTheme): CSSProperties {
+
+        const globals = this.globalStyles.map(global => Style.globalThemedStyles[global].compose(theme));
+
+        return lodash.merge({}, ...globals, this.shared, this.themed[theme])
+    }
+};
