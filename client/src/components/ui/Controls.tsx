1d15fa2c8190e86a47d4a2d952bd2f0954d8fe38
Jozef Méry <jozefdmery@gmail.com>
2020-04-12 22:00:32 +0200

    converted to hook api, added styles and tooltips

--- client/src/components/ui/Controls.tsx
+++ client/src/components/ui/Controls.tsx
@@ -8,54 +8,123 @@
  * 
  */
 
-// import dependencies
-import React, { Component } from "react";
-import { capitalize } from "../../utils";
-import classNames from "classnames";
+// import react
+import React from "react";
 
-// import react-redux
-import { connect } from "react-redux";
+// import redux utilities and slices
+import { useSelector, useDispatch } from "react-redux";
+import { toggleSimRunning } from "../../state/simSlice";
 
-// import redux slices
-import simSlice from "../../state/simSlice";
+// import UI elements
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
+import PlayIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
 
-// import custom components
-import Button from "./Button";
+// import stylers
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import { Style } from "../../stylers";
 
 // import type information
 import { StateShape } from "../../state/defaultState";
 
 // import language data
-import LanguageData from "../../lang/all";
+import languageData from "../../lang/all";
 
-type ControlsProps = Pick<StateShape, "language" | "sim" | "theme"> & typeof simSlice.actions;
+// define styles
+const UIPanelStyle: Style = Style.create({
 
-class Controls extends Component<ControlsProps> {
+    // position
+    position: "fixed",
 
-    /// Public static methods
+    // center horizontally
+    left: "50%",
+    transform: "translate(-50%)",
+    bottom: "0px",
 
-    public static stateToProps = ({ language, sim, theme }: StateShape) => ({ language, sim, theme });
-   
-    /// Public methods
-    
-    public render() {
+    // spacing
+    padding: "20px",
 
-        // --- shorthands 
-        const simRunning = this.props.sim.running;
-        const languageData = LanguageData[this.props.language];
-        const theme = this.props.theme;
-        // --- shorthands
+    // style
+    borderRadius: "3px 3px 0 0",
+    borderStyle: "solid solid none solid"
 
-        let text = capitalize((simRunning) ? languageData["pause"] : languageData["play"]);
+}, {}, ["UIPanel", "VerticalFlexBox", "ColorTransition"]);
 
-        return (
-            <div id="bottom-controls" className={classNames(theme, "ui-panel")}>
-                    <Button content={() => text} id="" classNames={theme} 
-                            tooltip={text}
-                            onClick={() => this.props.toggleSimRunning()}
-                    />
-            </div>);
+const buttonStyle: Style = Style.create({
+
+    "&:not(:last-child)": {
+
+        marginRight: "20px"
+    },
+
+}, {}, ["ControlButton", "ColorTransition"]);
+
+const tooltipStyle: Style = Style.create(undefined, undefined, "SimpleTooltip");
+
+const useControlStyles = makeStyles(({ theme }: Theme) => ({
+
+    panel: UIPanelStyle.compose(theme),
+    button: buttonStyle.compose(theme),
+    tooltip: tooltipStyle.compose(theme)
+}));
+
+type ControlElements = "playToggler";
+
+const mapStateToProps = ({ language, sim }: StateShape) => ({ language, sim });
+type ControlsProps = ReturnType<typeof mapStateToProps>;
+
+// define helper functions
+function getIcon(props: ControlsProps, element: ControlElements): React.ReactNode {
+
+    // --- shorthands
+    const simRunning = props.sim.running;
+    // --- shorthands
+
+    switch(element) {
+
+        case "playToggler":
+
+            return simRunning ? <PauseIcon /> : <PlayIcon />;
     }
 }
- 
-export default connect(Controls.stateToProps, { ...simSlice.actions })(Controls);
+
+function getTooltip(props: ControlsProps, element: ControlElements): string {
+
+    // --- shorthands
+    const simRunning = props.sim.running;
+    const currentLanguage = languageData[props.language];
+    // --- shorthands
+
+    switch(element) {
+
+        case "playToggler":
+
+            return simRunning ? currentLanguage.pause : currentLanguage.play;
+    }
+}
+
+// define react element
+export default function Controls() {
+
+    // get data from redux store
+    const props = useSelector(mapStateToProps);
+
+    // get style classes
+    const classes = useControlStyles();
+
+    const dispatch = useDispatch();
+
+    return (
+        <div className={classes.panel}>
+            <Tooltip title={getTooltip(props, "playToggler")} 
+                    placement="top" 
+                    TransitionComponent={Zoom}
+                    classes={{ tooltip: classes.tooltip }}>
+                <Button className={classes.button} onClick={() => dispatch(toggleSimRunning()) }>
+                    {getIcon(props, "playToggler")}
+                </Button>
+            </Tooltip>
+        </div>);
+}
--- client/src/components/ui/TopBar.tsx
+++ client/src/components/ui/TopBar.tsx
@@ -8,47 +8,122 @@
  * 
  */
 
-// import dependencies
-import React, { Component } from "react";
-import classNames from "classnames";
+// import react
+import React from "react";
 
-// import react-redux
-import { connect } from "react-redux";
+// import redux utilities and slices
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../../state/themeSlice";
 
-// import redux slices
-import themeSlice from "../../state/themeSlice";
+// import UI elements
+import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
+import LightIcon from "@material-ui/icons/Brightness7";
+import DarkIcon from "@material-ui/icons/Brightness4";
 
-// import custom components
-import ToggleButton from "./ToggleButton";
+// import stylers
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import { Style, ColorTheme } from "../../stylers";
 
 // import type information
 import { StateShape } from "../../state/defaultState";
 
-type TopBarProps = Pick<StateShape, "theme"> & typeof themeSlice.actions;
+// import language data
+import languageData from "../../lang/all";
 
-class TopBar extends Component<TopBarProps> {
-    
-    /// Public static methods
+// define styles
+const UIPanelStyle: Style = Style.create({
 
-    public static stateToProps = ({ theme }: StateShape) => ({ theme });
-   
-    /// Private methods
+    position: "fixed",
+    // stretch vertically
+    top: "0px",
+    left: "0px",
+    right: "0px",
 
-    private isThemeToggled() {
+    // spacing
+    padding: "10px",
 
-        return this.props.theme === "dark";
-    } 
+    // style
+    borderStyle: "none",
+    borderBottomStyle: "solid"
 
-    /// Public methods
+}, {}, ["UIPanel", "ColorTransition", "VerticalFlexBox"]);
 
-    public render() { 
+const buttonStyle: Style = Style.create({
 
-        return (
-            <div id="top-bar" className={classNames(this.props.theme, "ui-panel")}>
-                <ToggleButton classNames={this.props.theme} onClick={() => this.props.toggleTheme()} isToggled={this.isThemeToggled()} />
-            </div>
-                );
+    "&:first-child": {
+
+        marginLeft: "auto"
+    }
+}, undefined, ["TopBarButton", "ColorTransition"]);
+
+const tooltipStyle: Style = Style.create(undefined, undefined, "SimpleTooltip");
+
+const useTopBarStyles = makeStyles(({ theme }: Theme) => ({
+
+    panel: UIPanelStyle.compose(theme),
+    button: buttonStyle.compose(theme),
+    tooltip: tooltipStyle.compose(theme)
+}));
+
+type TopBarElements = "themeToggler";
+
+const mapStateToProps = ({ theme, language }: StateShape) => ({ theme, language });
+type TopBarProps = ReturnType<typeof mapStateToProps>;
+
+function getIcon(props: TopBarProps, element: TopBarElements): React.ReactNode {
+
+    // --- shorthands 
+    const theme = props.theme;
+    // --- shorthands
+
+    const themeIsDark = theme === ColorTheme.DARK;
+
+    switch(element) {
+
+        case "themeToggler":
+
+            return themeIsDark ? <LightIcon /> : <DarkIcon />;
     }
 }
 
-export default connect(TopBar.stateToProps, { ...themeSlice.actions })(TopBar);
+function getTooltip(props: TopBarProps, element: TopBarElements): string {
+
+    // --- shorthands 
+    const theme = props.theme;
+    const currentLanguage = languageData[props.language];
+    // --- shorthands
+
+    const themeIsDark = theme === ColorTheme.DARK;
+
+    switch(element) {
+
+        case "themeToggler":
+
+            return themeIsDark ? currentLanguage["setLightTheme"] : currentLanguage["setDarkTheme"];
+    }
+}
+
+export default function TopBar() {
+
+    // get data from redux store
+    const props = useSelector(mapStateToProps);
+
+    // get style classes
+    const classes = useTopBarStyles();
+
+    const dispatch = useDispatch();
+
+    return (
+        <div className={classes.panel}>
+            <Tooltip title={getTooltip(props, "themeToggler")} 
+                    placement="top" 
+                    TransitionComponent={Zoom}
+                    classes={{ tooltip: classes.tooltip }}>
+                <Button className={classes.button} onClick={() => dispatch(toggleTheme()) }>
+                    {getIcon(props, "themeToggler")}
+                </Button>
+            </Tooltip>
+        </div>);
+}
