8728b0a08666041788c962f14f7eef461b9ca0d8
Jozef Méry <jozefdmery@gmail.com>
2020-04-15 18:33:04 +0200

    added various helper types

---
+++ client/src/types.ts
@@ -0,0 +1,21 @@
+/**
+ * File: types.ts
+ * 
+ * Author: Jozef Méry <xmeryj00@stud.fit.vutbr.cz>
+ * Date: 15.4.2020
+ * License: none
+ * Description: Defines various helper types.
+ * 
+ */
+
+export type Position2D = {
+
+    x: number;
+    y: number;
+};
+
+export type Dimensions2D = {
+
+    width: number;
+    height: number;
+};
