7eaef5b4f13e1384aaa5aeca359e895d8bb15a5b
Jozef Méry <jozefdmery@gmail.com>
2020-04-19 19:12:25 +0200

    created reusable hook for language strings

---
+++ client/src/hooks/UseLanguageString.ts
@@ -0,0 +1,25 @@
+/**
+ * File: UseLanguageString.ts
+ * 
+ * Author: Jozef Méry <xmeryj00@stud.fit.vutbr.cz>
+ * Date: 19.4.2020
+ * License: none
+ * Description: Defines a hook for using localized language strings.
+ * 
+ */
+
+// import redux utilities 
+import { useSelector } from "react-redux";
+
+// import language data
+import languageData, { LanguageStrings } from "../lang/all";
+
+// import type information
+import { StateShape } from "../state/defaultState";
+
+export function useLanguageString(str: LanguageStrings) {
+
+    const selectedLanguage = useSelector((state: StateShape) => state.global.language);
+
+    return languageData[selectedLanguage][str];
+}
