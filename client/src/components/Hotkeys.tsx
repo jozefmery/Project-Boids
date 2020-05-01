652b39594765298636bb3bae8ec2220ff6c5abab
Jozef Méry <jozefdmery@gmail.com>
2020-05-02 00:14:54 +0200

    added dynamicly updating actions and sequences

--- client/src/components/Hotkeys.tsx
+++ client/src/components/Hotkeys.tsx
@@ -11,8 +11,14 @@
 // import react
 import React, { useRef, useCallback, useEffect } from "react";
 
+// import actions
+import { useAction, Action } from "../actions";
+
 // import hotkeys
 import Context from "@dodmeister/hotkeys";
+
+// import type information
+import { ActionHotkeys } from "../state/types";
 
 export const HotkeyContext = React.createContext<Context>(null as any);
 
@@ -20,11 +26,49 @@ type KeyCaptureContextProps = {
 
     children: React.ReactNode;
     tabIndex?: number;
+    hotkeys?: ActionHotkeys;
 };
 
-export function KeyCaptureContext({ children, tabIndex = 0 }: KeyCaptureContextProps) {
+export function KeyCaptureContext({ children, tabIndex = 0, hotkeys = {} }: KeyCaptureContextProps) {
 
     const context = useRef(new Context());
+
+    const actionMap = useAction();
+
+    useEffect(() => {
+
+        let action: Action;
+
+        for(action in hotkeys) {
+
+            let sequences: string | Array<string>;
+
+            if(hotkeys[action] !== undefined) {
+
+                // safety check provided above
+                // safely cast to sequence definitions
+                sequences = hotkeys[action] as string | Array<string>;
+
+            } else {
+                
+                sequences = [];
+            }
+
+            const handle = context.current.get(action);
+
+            if(handle) {
+
+                // update sequences and callbacks
+                handle.setSequences(sequences);
+                handle.setCallback(actionMap[action]);
+
+            } else {
+
+                context.current.add({ sequences, callback: actionMap[action], id: action });
+            }
+        }
+
+    }, [hotkeys, context, actionMap]);
 
     const cleanupContext = useCallback(() => {
 
