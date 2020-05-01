a6016da2b1299bc09fe3b9dac73e68b737a15cc9
Jozef Méry <jozefdmery@gmail.com>
2020-05-02 00:03:54 +0200

    fixed import,  state store is now default exported

--- client/src/state/stateStore.ts
+++ client/src/state/stateStore.ts
@@ -18,16 +18,16 @@ import defaultState from "./defaultState";
 // import reducers
 import sim from "./simSlice";
 import global from "./globalSlice";
-import keyboard from "./keyboardSlice";
+import hotkeys from "./hotkeysSlice";
 
 const reduxStateStore = configureStore({
     
     // merge slice reducers into a root reducer
-    reducer: { sim, global, keyboard },
+    reducer: { sim, global, hotkeys },
     // enable extension for debugging
     devTools: true,
     // load default state
     preloadedState: defaultState
 });
 
-export { reduxStateStore };
+export default reduxStateStore;
