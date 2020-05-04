e4aed60152f158fd076e2eb54f9aa4d090b58189
Jozef Méry <jozefdmery@gmail.com>
2020-05-04 18:00:23 +0200

    changed server to serve client

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-
-const items = require("./routes/api/items");
+const path = require("path");
 
 const app = express();
 
-app.use(bodyParser.json());
-
-const db = require("./config/keys").mongoURI;
-
-// connect to db
-mongoose.connect(db)
-    .then(() => console.log("Database connected"))
-    .catch(err => console.log("Failed to connect to db: " + err)); 
-
-// use route
-app.use("/api/items", items);
+app.use(express.static(path.join(__dirname, "client")));
 
 const port = process.env.PORT || 5000;
+
+app.get("/*", (_, res) => res.sendFile(path.join(__dirname, "client/index.html")));
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
