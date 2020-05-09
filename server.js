81d6d3cd1463920dd2da5c11cfa7bf7c36fbb5c5
Jozef Méry <jozefdmery@gmail.com>
2020-05-09 18:16:27 +0200

    added option to change default port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "client")));
 
-const port = process.env.PORT || 5000;
+const port = process.argv[2] || 5000;
 
 app.get("/*", (_, res) => res.sendFile(path.join(__dirname, "client/index.html")));
 
