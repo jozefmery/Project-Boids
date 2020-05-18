9aa3df6e326b75073db28dae5838424ef89a4cc6
Jozef Méry <jozefdmery@gmail.com>
2020-05-18 15:50:55 +0200

    added  checks for port number

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "client")));
 
-const port = process.argv[2] || 5000;
+const DEFAULT_PORT = 5000;
+
+const portParam = new RegExp(/^\d+$/).test(process.argv[2]) ? process.argv[2] : undefined;
+
+const port = parseInt(portParam) || DEFAULT_PORT;
 
 app.get("/*", (_, res) => res.sendFile(path.join(__dirname, "client/index.html")));
 
