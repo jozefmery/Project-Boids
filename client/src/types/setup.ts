a5f50a50ac79d918a0deb94e10f43087e710cc1e
Jozef Méry <jozefdmery@gmail.com>
2020-06-01 16:35:54 +0200

    moved setup types to separate file

---
+++ client/src/types/setup.ts
@@ -0,0 +1,48 @@
+/**
+ * File: types/setup.ts
+ * 
+ * Author: Jozef Méry <xmeryj00@stud.fit.vutbr.cz>
+ * Date: 28.4.2020
+ * License: none
+ * Description: Defines types for the simulation setup component.
+ * 
+ */
+
+export type InputWithValidation = {
+
+    valid: boolean;
+    value: string;
+};
+
+export type ValidatedEntityOptions = {
+
+    [key in     "initialCount"  
+            |   "speed"
+            |   "maxForceAngle"
+            |   "maxForceMagnitude"
+            |   "perceptionRadius"
+            |   "perceptionAngle"
+            |   "alignmentModifier"
+            |   "cohesionModifier"
+            |   "separationModifier"
+            |   "hungerDecay"
+            |   "healthDelta"
+            |   "health"
+            |   "hunger"
+            |   "reproductionInterval"
+            |   "maxAge"
+            |   "eatingThreshold"
+            |   "mutationModifier"]: InputWithValidation; 
+};
+
+export type SetupState = {
+
+    drawQuadTree: boolean;
+    area: number;
+    foodSpawnRate: InputWithValidation;
+    foodMaxAge: InputWithValidation;
+    initialFood: InputWithValidation;
+
+    predators: ValidatedEntityOptions;
+    preys: ValidatedEntityOptions;
+};
